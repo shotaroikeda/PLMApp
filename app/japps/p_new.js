d615d9564940ab8de0b24ba83a88ead2797cc912
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-08-14 18:28:11 +0900

    added information

---
+++ app/japps/p_new.js
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+// This is a rewrite of paint.js							 //
+// 											 //
+// The problem with the current paint.js is that it does not follow readable code	 //
+// standards. In addition some functions are/were generally inefficent and should be	 //
+// rewritten from the ground up.							 //
+// 											 //
+// The original will stay there for backwards compatibility, until this one is complete. //
+///////////////////////////////////////////////////////////////////////////////////////////
+
