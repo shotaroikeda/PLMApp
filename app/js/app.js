5e1a4d61784358f1b29ab505886c2d4346c1f7a7
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-08-19 22:53:58 +0900

    wrote index and app to support angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,21 @@
 var app = angular.module('PLMApp', ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
-        .when('/', {
-            controller: 'SelectionController',
-            templateUrl: 'views/selection.html'
-        })
+    /*
+      add this when done
+      .when('/', {
+      controller: 'SelectionController',
+      templateUrl: 'views/selection.html'
+      })
+    */
         .when('/index', {
             controller: 'SelectionController',
             templateUrl: 'views/selection.html'
         })
-	.when('/selection', {
-	    controller: 'SelectionController',
-	    templateUrl: 'views/selection.html'
-	})
+        .when('/selection', {
+            controller: 'SelectionController',
+            templateUrl: 'views/selection.html'
+        })
     /* Quick Note:
        When we having a working backend, this will change quite a bit.
        It will go from /detail/ to stuff like /index/:id_detail/:id_story/...
@@ -20,19 +23,19 @@ app.config(function ($routeProvider) {
 
        For now we'll have static URLs.
     */
-	.when('/storyboard', {
-	    controller: 'StoryboardController',
-	    templateUrl: 'views/storyboard.html'
-	})
+        .when('/storyboard', {
+            controller: 'StoryboardController',
+            templateUrl: 'views/storyboard.html'
+        })
         .when('/detail', {
-	    controller: 'DetailController',
-	    templateUrl: 'views/detail.html'
-	})
-	.when('/canvas', {
-	    controller: 'CanvasController',
-	    templateUrl: 'views/canvas.html'
-	})
-	.otherwise({
-	    redirectTo: '/canvas' // Point to most recent section of development for convenience.
-	})
+            controller: 'DetailController',
+            templateUrl: 'views/detail.html'
+        })
+        .when('/canvas', {
+            controller: 'CanvasController',
+            templateUrl: 'views/canvas.html'
+        })
+        .otherwise({
+            redirectTo: '/canvas' // Point to most recent section of development for convenience.
+        });
 });
